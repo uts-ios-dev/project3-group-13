af1c26eaaa05f382c9e671922d906b6f5a1ea08a
Rhys Biddle <Rhys.Biddle@student.uts.edu.au>
2018-05-07 19:27:04 +1000

    add SmartMenu

---
+++ SmartMenu/SmartMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartMenu
+//
+//  Created by Rhys Biddle on 7/5/18.
+//  Copyright © 2018 Group-13. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
