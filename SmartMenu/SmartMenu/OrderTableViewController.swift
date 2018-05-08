14b950f2c238e011301e324180a4ab72ab439907
Oliver <oliverfchen@gmail.com>
2018-05-08 21:47:38 +1000

    tired

--- SmartMenu/SmartMenu/OrderTableViewController.swift
+++ SmartMenu/SmartMenu/OrderTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  MenuTableTableViewController.swift
+//  OrderTableViewController.swift
 //  SmartMenu
 //
 //  Created by UTS on 8/5/18.
@@ -8,103 +8,16 @@
 
 import UIKit
 
-class MenuTableTableViewController: UITableViewController {
-    
-    
-    
-    @IBAction func showOrder(_ sender: UIButton) {
-    }
-    
-    
-    
-    
-    
-    
-    
-//    enum AvailavlePriod {
-//        case
-//            Mooring,
-//            Lunch,
-//            Dinner,
-//            AllDay
-//    }
-//    enum AvailableDay {
-//        case
-//            Monday,
-//            Tuesday,
-//            Wednesday,
-//            Thursday,
-//            Friday,
-//            Saturday,
-//            Sunday
-//    }
-//
-//    struct AvaivaleType {
-//       // var availableDay = PriodSet.init(dayInWeek: 1, serveTime: 1)
-//       // var availavleTime = 1
-//    }
-    struct PriodSet {
-        var dayInWeek: Dictionary =
-        [
-            1: "Monday",
-            2: "Tuesday",
-            3: "Wednesday",
-            4: "Thursday",
-            5: "Friday",
-            6: "Saturday",
-            7: "Sunday"]
-        var serveTime: Dictionary =
-        [
-            1: "Mooring",
-            2: "Lunch",
-            3: "Dinner",
-            4: "All Time"]
-    }
-    
-    class Menu
-    {
-        var foodName: String
-        var foodImg: String
-        var foodDescription: String
-        var availablePriod: [Int] = []
-        var availableDay: [Int] 
-        var foodSelectedQty: Int
-        init (foodName: String, foodImg: String, foodDescription: String, availablePriod: [Int], availableDay: [Int], foodSelectedQty: Int)
-        {
-            self.foodName = foodName
-            self.foodImg = foodImg
-            self.foodDescription = foodDescription
-            //self.availablePriod = [PriodSet()]
-            self.availableDay = [Int]()
-            self.foodSelectedQty = foodSelectedQty
-        }
-    }
+class OrderTableViewController: UITableViewController {
 
-    var menus: [Menu] = [
-    
-    
-        Menu(foodName: "Green Tea",
-              foodImg: "blur-breakfast-close-up-376464.jpg",
-              foodDescription: "good to eat",
-              availablePriod: [1,2,3],
-              availableDay: [3,4,6],
-              //availablePriod: (PriodSet.dayInWeek: 1, PriodSet.serveTime: 2),
-              foodSelectedQty: 0
-        ),
-        Menu(foodName: "Coffee", foodImg: "", foodDescription: "drink", availablePriod: [2,3], availableDay: [3,4], foodSelectedQty: 1)
-    ]
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print ("print menu list")
-        for item in menus {
-            print (item.foodName, item.availableDay.count)
-            for list in item.availableDay{
-                print ("list: \(list)")
-            }
-        }
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     override func didReceiveMemoryWarning() {
