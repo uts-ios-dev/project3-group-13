a95938eabbc6486e6f225f8ce3d67ed4a0385317
Rhys Biddle <Rhys.Biddle@student.uts.edu.au>
2018-05-09 18:54:37 +1000

    add Order data model

---
+++ SmartMenu/SmartMenu/Order.swift
@@ -0,0 +1,13 @@
+//
+//  Order.swift
+//  SmartMenu
+//
+//  Created by Rhys Biddle on 9/5/18.
+//  Copyright © 2018 Group-13. All rights reserved.
+//
+
+import Foundation
+
+struct Order{
+    let orderItems : [OrderItem]
+}
