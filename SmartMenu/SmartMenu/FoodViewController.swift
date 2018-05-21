c43b729e000e4c0b6943ce59ba91a884622d4771
Oliver <oliverfchen@gmail.com>
2018-05-21 11:00:03 +1000

    add add button

--- SmartMenu/SmartMenu/FoodViewController.swift
+++ SmartMenu/SmartMenu/FoodViewController.swift
@@ -10,14 +10,36 @@ import UIKit
 
 class FoodViewController: UIViewController {
     var food: Food!
-    
+    var ordered: Int = 0
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var order: UILabel!
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         assert(food != nil)
         nameLabel.text = food.name
         descriptionLabel.text = food.description
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+      order.text = "Order: " + String(ordered)
+    }
+@IBAction func addOrder(_ sender: UIBarButtonItem) {
+    ordered += 1
+    print (ordered)
+    order.text = "Order: " + String(ordered)
+}
+
+@IBAction func reduceOrder(_ sender: UIBarButtonItem) {
+    if ordered > 0 {
+        ordered -= 1
+    order.text = "Order: " + String(ordered)
+    print (ordered)
+        
+        }
     }
 }
